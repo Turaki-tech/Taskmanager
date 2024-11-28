349714701a711954d8c05a277a168572b8bb02db
Turaki-tech <sameenyunusa@gmail.com>
2024-11-28 15:13:20 -0800

    update server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+// server.js
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+const authMiddleware = require('./middleware/authMiddleware');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/tasks', authMiddleware, taskRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
